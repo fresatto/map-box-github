05c6b7fe6987bdd1dec0167a46305b052423301f
Gabriel Fresatto <gabrielfresatto@gmail.com>
2019-06-05 11:29:44 -0300

    Updates

---
+++ precache-manifest.4156a8c034b5d59e8e0d68876f45e2b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d941b6d0bc11c051ce57a3aa198302a",
+    "url": "/map-box-github/index.html"
+  },
+  {
+    "revision": "5d1254e509807e859207",
+    "url": "/map-box-github/static/css/main.47432c27.chunk.css"
+  },
+  {
+    "revision": "ee266c177f6303bb0166",
+    "url": "/map-box-github/static/js/2.094125a0.chunk.js"
+  },
+  {
+    "revision": "5d1254e509807e859207",
+    "url": "/map-box-github/static/js/main.83a44b98.chunk.js"
+  },
+  {
+    "revision": "11d5142eaae33536f28f",
+    "url": "/map-box-github/static/js/runtime~main.69ef5ba5.js"
+  }
+]);
